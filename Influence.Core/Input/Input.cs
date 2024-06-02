8d2036f89e19d7cb81803b118b5220466198cc3d
Quartzi <34374881+realQuartzi@users.noreply.github.com>
2024-06-02 14:47:29 +0200

    Create Input.cs

---
+++ Influence.Core/Input/Input.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+using Silk.NET.Input;
+
+using Influence.Core;
+
+namespace Influence.Input
+{
+    public static class Input
+    {
+        static IInputContext inputContext;
+        static Dictionary<Key, bool> keyStates = new Dictionary<Key, bool>();
+
+        internal static void Initialize(WindowContext window)
+        {
+            inputContext = window.Window.CreateInput();
+
+            foreach(var keyboard in inputContext.Keyboards)
+            {
+                keyboard.KeyDown += OnKeyDown;
+                keyboard.KeyUp += OnKeyUp;
+            }
+        }
+
+        static void OnKeyDown(IKeyboard keyboard, Key key, int keyCode) => keyStates[key] = true;
+        static void OnKeyUp(IKeyboard keyboard, Key key, int keyCode) => keyStates[key] = false;
+
+        public static bool IsKeyDown(Key key) => keyStates.GetValueOrDefault(key, false);
+        public static bool IsKeyUp(Key key) => !keyStates.GetValueOrDefault(key, false);
+    }
+}
