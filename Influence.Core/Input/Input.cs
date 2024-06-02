ddd43ff804bbadc67f9e9ca623f85156fcc0fc23
Quartzi <34374881+realQuartzi@users.noreply.github.com>
2024-06-02 14:55:50 +0200

    Added Mouse location to Input

--- Influence.Core/Input/Input.cs
+++ Influence.Core/Input/Input.cs
@@ -1,25 +1,31 @@
 ﻿using System.Collections.Generic;
 
 using Silk.NET.Input;
+using Silk.NET.Windowing;
 
-using Influence.Core;
-
-namespace Influence.Input
+namespace Influence
 {
     public static class Input
     {
         static IInputContext inputContext;
         static Dictionary<Key, bool> keyStates = new Dictionary<Key, bool>();
 
-        internal static void Initialize(WindowContext window)
+        static IMouse mouse;
+        public static Vector2 mousePosition => new Vector2(mouse.Position.X, mouse.Position.Y);
+
+        internal static void Initialize(IWindow window)
         {
-            inputContext = window.Window.CreateInput();
+            inputContext = window.CreateInput();
 
             foreach(var keyboard in inputContext.Keyboards)
             {
                 keyboard.KeyDown += OnKeyDown;
                 keyboard.KeyUp += OnKeyUp;
             }
+
+            mouse = inputContext.Mice[0];
+            mouse.Cursor.CursorMode = CursorMode.Raw;
+            // Create hookable Action where which you can register too for MouseMove Event
         }
 
         static void OnKeyDown(IKeyboard keyboard, Key key, int keyCode) => keyStates[key] = true;
