5bf8ef8c894b871aef4309226993b1c4333caff1
Quartzi <34374881+realQuartzi@users.noreply.github.com>
2024-06-03 00:40:06 +0200

    Added keyboard and mice collection

--- Influence.Core/Input/Input.cs
+++ Influence.Core/Input/Input.cs
@@ -8,13 +8,16 @@ namespace Influence
     public static class Input
     {
         static IInputContext inputContext; // Current Input Context
-        internal static IKeyboard activeKeyboard; // Last used Keyboard
-        internal static IMouse activeMouse; // Last used Mouse
+
+        static IKeyboard activeKeyboard; // Last used Keyboard
+        internal static IReadOnlyList<IKeyboard> keyboards; // All active Keyboards
+
+        static IMouse activeMouse; // Last used Mouse
+        internal static IReadOnlyList<IMouse> mice; // All active Mice
 
         static Dictionary<int, bool> keyStates = new Dictionary<int, bool>();
 
         public static Vector2 mousePosition => new Vector2(activeMouse.Position.X, activeMouse.Position.Y);
-
 
         internal static void Initialize(IWindow window)
         {
@@ -24,8 +27,10 @@ namespace Influence
 
         static void RegisterInputs()
         {
+            keyboards = inputContext.Keyboards;
+
             // Register key Events on all keyboards.
-            foreach (IKeyboard keyboard in inputContext.Keyboards)
+            foreach (IKeyboard keyboard in keyboards)
             {
                 // We remove the event to ensure we don't have it multiple times.
                 keyboard.KeyDown -= OnKeyDown;
@@ -36,23 +41,24 @@ namespace Influence
             }
 
             // Default to the first Keyboard as our "active"
-            if(inputContext.Keyboards.Count > 0)
-                activeKeyboard = inputContext.Keyboards[0];
+            if(keyboards.Count > 0)
+                activeKeyboard = keyboards[0];
+
+            mice = inputContext.Mice;
 
             // Register button Events on all mice.
-            foreach (IMouse mouse in inputContext.Mice)
+            foreach (IMouse mouse in mice)
             {
                 mouse.MouseDown -= OnMouseDown;
                 mouse.MouseUp -= OnMouseUp;
 
                 mouse.MouseDown += OnMouseDown;
                 mouse.MouseUp += OnMouseUp;
-                mouse.Cursor.CursorMode = CursorMode.Raw;
             }
 
             // Default to the first Mouse as our "active"
-            if(inputContext.Mice.Count > 0)
-                activeMouse = inputContext.Mice[0];
+            if(mice.Count > 0)
+                activeMouse = mice[0];
         }
 
         #region Keyboard Functions
