08aa05e2a655e178c86a4fcf3842f8dee7ddc7a7
Quartzi <34374881+realQuartzi@users.noreply.github.com>
2024-06-10 20:36:58 +0200

    Add Global Light

---
+++ Influence.Graphics/Graphics/GlobalLight.cs
@@ -0,0 +1,29 @@
+﻿
+namespace Influence.Graphics;
+
+public static class GlobalLight
+{
+    static Vector3 _position = rotation.ToEuler().normalized * 8;
+    internal static Vector3 position
+    {
+        get { return _position; }
+        private set { _position = value; }
+    }
+
+    static Quaternion _rotation = Quaternion.Euler(Vector3.Right);
+    public static Quaternion rotation
+    {
+        get { return _rotation; }
+        set
+        {
+            _rotation = value;
+            position = value.ToEuler().normalized * 8;
+        }
+    }
+
+    public static Color color = Color.Blue;
+    public static float intensity = 1f;
+
+    public static float specularStrength = 0.5f;
+    public static float ambientStrength = 0.8f;
+}
