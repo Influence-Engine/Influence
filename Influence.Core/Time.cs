c920d712020b36c6630e92dda21f49de0a0045bd
Quartzi <34374881+realQuartzi@users.noreply.github.com>
2024-10-19 22:04:03 +0200

    Time class added

---
+++ Influence.Core/Time.cs
@@ -0,0 +1,11 @@
+﻿namespace Influence
+{
+    public static class Time
+    {
+        public static double time;
+        public static double deltaTime;
+
+        public static double fixedTime;
+        public static double fixedDeltaTime;
+    }
+}
