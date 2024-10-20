e2e407f018d4fe38a706eb03234ee6bafff5d19a
Quartzi <34374881+realQuartzi@users.noreply.github.com>
2024-10-20 20:02:07 +0200

    Added Render Class

---
+++ Influence.Core/Renderer.cs
@@ -0,0 +1,43 @@
+﻿using SDL3;
+using System;
+
+namespace Influence.Core
+{
+    public class Renderer
+    {
+        IntPtr rendererPtr;
+        public IntPtr RendererHandle => rendererPtr;
+
+        SDL.Rect viewport;
+
+        public Renderer(IntPtr windowPtr, int width, int height)
+        {
+#if DEBUG
+            Console.WriteLine($"Creating Renderer: Window Handle => {windowPtr.ToInt64()} ({width}x{height})");
+#endif
+
+            rendererPtr = SDL.CreateRenderer(windowPtr, 0, SDL.RendererFlags.Accelerated);
+
+            if (rendererPtr == IntPtr.Zero)
+            {
+                Console.WriteLine("Unable to create a renderer. Error: " + SDL.GetError());
+                return;
+            }
+
+#if DEBUG
+            Console.WriteLine("New Renderer Handle: " + rendererPtr.ToInt64().ToString());
+#endif
+
+            viewport = new SDL.Rect();
+            viewport.w = width;
+            viewport.h = height;
+
+            SDL.SetRenderViewport(rendererPtr, ref viewport);
+        }
+
+        ~Renderer()
+        {
+            SDL.DestroyRenderer(rendererPtr);
+        }
+    }
+}
