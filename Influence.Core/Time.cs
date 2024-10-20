8455a9dbcd802a5a34dd9231271a9560a0f0088b
Quartzi <34374881+realQuartzi@users.noreply.github.com>
2024-10-20 20:01:29 +0200

    Updated Time class

--- Influence.Core/Time.cs
+++ Influence.Core/Time.cs
@@ -1,11 +1,31 @@
-﻿namespace Influence
+﻿using SDL3;
+
+namespace Influence
 {
     public static class Time
     {
+        /// <summary>Time in seconds since the Framework has started.</summary>
         public static double time;
+        /// <summary>Time interval in seconds from the last frame to the current one.</summary>
         public static double deltaTime;
 
-        public static double fixedTime;
-        public static double fixedDeltaTime;
+        /// <summary>Time in nanoseconds since the Framework has started.</summary>
+        public static ulong preciseTime;
+        /// <summary>Time interval in nanoseconds from the last frame to the current one.</summary>
+        public static ulong preciseDeltaTime;
+
+
+        /// <summary>Time passed in seconds since the end of last frame.</summary>
+        public static double frameTime
+        {
+            get
+            {
+                ulong deltaTicks = SDL.GetTickNS() - preciseTime;
+                return deltaTicks * 1.0e-9;
+            }
+        }
+
+        /// <summary>Time passed in nanoseconds since the end of last frame.</summary>
+        public static ulong preciseFrameTime => SDL.GetTickNS() - preciseTime;
     }
 }
