249c1d1e5003d08d0e53a6720ac6c24c442b663e
Quartzi <34374881+realQuartzi@users.noreply.github.com>
2024-10-22 02:29:07 +0200

    KeyCodes

---
+++ Influence.Core/Types/KeyCode.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Influence
+{
+    public enum KeyCode
+    {
+        None, // None
+        A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, // Alpha
+        One, Two, Three, Four, Five, Six, Seven, Eight, Nine, Zero, // Numbers
+        LeftClick, RightClick, MiddleClick, // Mouse
+        UpArrow, DownArrow, LeftArrow, RightArrow, // Arrow Keys
+        Backspace,
+        Delete,
+        Tab,
+        Return,
+        Escape,
+        Space,
+
+        RightShift, LeftShift,
+        RightAlt, LeftAlt,
+        RightControl, LeftControl
+    }
+}
